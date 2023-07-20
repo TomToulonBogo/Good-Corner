6a56f1dd1a1407613c0d544a95509fd7586c70bc
Tom <tom.toulon-bogo@3wa.io>
2023-07-20 22:48:52 +0200

    add all necessary pages for the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { compileClassMetadata } from '@angular/compiler';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingsPageComponent } from './new-listings-page/new-listings-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/listings', pathMatch: 'full'},
+  {path: 'listings', component: ListingsPageComponent, pathMatch: 'full'},
+  {path: 'listings/:id', component: ListingsPageComponent},
+  {path: 'contact/:id', component: ContactPageComponent}, 
+  {path: 'edit-listing/:id', component: EditListingPageComponent},
+  {path: 'my-listings', component: MyListingsPageComponent},
+  {path: 'new-listing', component: NewListingsPageComponent}, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
